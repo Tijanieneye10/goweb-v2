35ed717b60ff510b7904117ceb140fe4765161d1
Tijani Eneye Usman <brainyworld10@gmail.com>
2026-01-22 19:11:48 +0100

    wip

--- render/show.go
+++ render/show.go
@@ -4,7 +4,15 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"sync"
 )
+
+type TemplateCache struct {
+	cache       map[string]*template.Template
+	mutex       sync.RWMutex
+	isDev       bool
+	templateDir string
+}
 
 func Render(w http.ResponseWriter, filename string, data interface{}) {
 
@@ -21,4 +29,8 @@ func Render(w http.ResponseWriter, filename string, data interface{}) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func RenderWithCache(w http.ResponseWriter, filename string, data interface{}) {
+
 }
