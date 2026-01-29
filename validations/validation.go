fa7b6f8bae2a2ab7d503a1b6347c6cb3c1eee12a
Tijani Eneye Usman <brainyworld10@gmail.com>
2026-01-29 15:33:00 +0100

    wip

--- validations/validation.go
+++ validations/validation.go
@@ -6,6 +6,14 @@ func (e Error) Add(field, message string) {
 	e[field] = append(e[field], message)
 }
 
-func (e Error) Get(field string) []string {
-	return e[field]
+func (e Error) Get(field string) string {
+	es := e[field]
+	if len(es) == 0 {
+		return ""
+	}
+	return es[0]
+}
+
+func (e Error) Has(field string) bool {
+	return len(e[field]) > 0
 }
