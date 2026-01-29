18cd0270b29a9accc3047cef58e320709988313b
Tijani Eneye Usman <brainyworld10@gmail.com>
2026-01-29 15:50:37 +0100

    wip

--- validations/validation.go
+++ validations/validation.go
@@ -1,12 +1,18 @@
 package validations
 
-type Error map[string][]string
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
 
-func (e Error) Add(field, message string) {
+type error map[string][]string
+
+func (e error) Add(field, message string) {
 	e[field] = append(e[field], message)
 }
 
-func (e Error) Get(field string) string {
+func (e error) Get(field string) string {
 	es := e[field]
 	if len(es) == 0 {
 		return ""
@@ -14,6 +20,36 @@ func (e Error) Get(field string) string {
 	return es[0]
 }
 
-func (e Error) Has(field string) bool {
+func (e error) Has(field string) bool {
 	return len(e[field]) > 0
+}
+
+type Form struct {
+	url.Values
+	Error error
+}
+
+func NewForm(data url.Values) *Form {
+	return &Form{
+		Values: data,
+		Error:  error(make(map[string][]string)),
+	}
+}
+
+func (f *Form) Required(fields ...string) *Form {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Error.Add(field, fmt.Sprintf("%s is required", field))
+		}
+	}
+
+	return f
+}
+
+func (f *Form) MaxLength(field string, n int) *Form {
+	value := f.Get(field)
+	if len(value) > n {
+		f.Error.Add(field, fmt.Sprintf("%s can't be more than %v", field, n))
+	}
 }
